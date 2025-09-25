71af30eda8b4ea6850b06d8af33657f6c31138d8
achmichael <achmadmichael03@gmail.com>
2025-09-25 23:35:13 +0700

    praktikum & tugas

--- lib/screen_arguments.dart
+++ lib/screen_arguments.dart
@@ -1,6 +1,14 @@
+// class ScreenArguments {
+//   final String title;
+//   final String subtitle;
+//   final String description;
+//   ScreenArguments(this.title, this.subtitle, this.description);
+// }
 class ScreenArguments {
+  final String cover;
   final String title;
-  final String subtitle;
   final String description;
-  ScreenArguments(this.title, this.subtitle, this.description);
+  final String shortDesc;
+
+  ScreenArguments(this.cover, this.title, this.description, this.shortDesc);
 }
